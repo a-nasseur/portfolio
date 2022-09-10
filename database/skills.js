fde60bb67eb6b10b744931d95bf17d6174a16056
abdelhak <a.nasseur.dev@gmail.com>
2022-09-10 23:23:41 +0100

    added JS and Mongodb skills

--- database/skills.js
+++ database/skills.js
@@ -1,13 +1,20 @@
-import django from '../public/icons8-django.svg'
-import react from '../public/react.svg'
-import postgres from '../public/icons8-postgresql.svg'
-import node from '../public/nodejs.svg'
-import material from '../public/icons8-material-ui.svg'
+import django from '../public/icons8-django.svg';
+import react from '../public/react.svg';
+import postgres from '../public/icons8-postgresql.svg';
+import node from '../public/nodejs.svg';
+import material from '../public/icons8-material-ui.svg';
+import JavaScript from '../public/icons8-javascript.svg';
+import mongoDB from '../public/icons8-mongodb.png';
 
 
 
 
 export const skills = [
+    {
+        id: 6,
+        icon: JavaScript,
+        background: '#FFD600'
+    },
     {
         id: 1,
         icon: react,
@@ -33,4 +40,9 @@ export const skills = [
         icon: postgres,
         background: '#0277BD'
     },
+    {
+        id: 7,
+        icon: mongoDB,
+        background: '#81C784'
+    }
 ]
